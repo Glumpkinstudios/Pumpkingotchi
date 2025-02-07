9522bdde26c0820b01e3e3a117d1f433aa795422
ItsMerume <itsmerumesmail@gmail.com>
2025-02-07 16:12:35 +0100

    Updates

---
+++ index.397cfeee.js
@@ -0,0 +1,2 @@
+document.getElementById("form").onsubmit=n=>{n.preventDefault(),window.location.href=`${window.location.origin}${window.location.pathname}gotchi?channel=${document.getElementById("sname").value}`};
+//# sourceMappingURL=index.397cfeee.js.map
